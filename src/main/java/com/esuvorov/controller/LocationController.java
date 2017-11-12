0aba8eaaf53eed2d9bfa417cac899a330078f007
Evgeniy Suvorov <eo.suvorov@gmail.com>
2017-11-13 00:23:33 +0300

    Create custom json object for average mark

--- src/main/java/com/esuvorov/controller/LocationController.java
+++ src/main/java/com/esuvorov/controller/LocationController.java
@@ -2,11 +2,10 @@ package com.esuvorov.controller;
 
 import com.esuvorov.model.Location;
 import com.esuvorov.service.LocationService;
-import lombok.Data;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.text.DecimalFormat;
 import java.util.List;
 
 @RestController
@@ -30,23 +29,12 @@ public class LocationController {
     }
 
     @GetMapping(value = "/{id}/avg")
-    public ResultAvg avg(@PathVariable Long id,
-                         @RequestParam(value = "fromDate", required = false) Long fromDate,
-                         @RequestParam(value = "toDate", required = false) Long toDate,
-                         @RequestParam(value = "fromAge", required = false) Long fromAge,
-                         @RequestParam(value = "toAge", required = false) Long toAge,
-                         @RequestParam(value = "gender", required = false) String gender) {
-        double averageMark = service.getAverageMark(id, fromDate, toDate, fromAge, toAge, gender);
-        return new ResultAvg(averageMark);
-    }
-
-    @Data
-    private class ResultAvg {
-        private double avg;
-
-        ResultAvg(double avg) {
-            String formattedAvgMark = new DecimalFormat(".##").format(avg);
-            this.avg = Double.parseDouble(formattedAvgMark);
-        }
+    public JSONObject avg(@PathVariable Long id,
+                          @RequestParam(value = "fromDate", required = false) Long fromDate,
+                          @RequestParam(value = "toDate", required = false) Long toDate,
+                          @RequestParam(value = "fromAge", required = false) Long fromAge,
+                          @RequestParam(value = "toAge", required = false) Long toAge,
+                          @RequestParam(value = "gender", required = false) String gender) {
+        return service.getAverageMark(id, fromDate, toDate, fromAge, toAge, gender);
     }
 }
--- src/main/java/com/esuvorov/service/LocationService.java
+++ src/main/java/com/esuvorov/service/LocationService.java
@@ -3,11 +3,15 @@ package com.esuvorov.service;
 import com.esuvorov.model.Location;
 import com.esuvorov.repository.LocationRepository;
 import org.apache.log4j.Logger;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.lang.Double.parseDouble;
 
 @Service
 public class LocationService {
@@ -29,7 +33,12 @@ public class LocationService {
         return repository.findById(id).orElse(null);
     }
 
-    public double getAverageMark(Long id, Long fromDate, Long toDate, Long fromAge, Long toAge, String gender) {
-        return repository.getAvgMarkByLocation(id, fromDate, toDate, fromAge, toAge, gender);
+    @SuppressWarnings("unchecked")
+    public JSONObject getAverageMark(Long id, Long fromDate, Long toDate, Long fromAge, Long toAge, String gender) {
+        double averageMark = repository.getAvgMarkByLocation(id, fromDate, toDate, fromAge, toAge, gender);
+        String formattedAverageMark = new DecimalFormat(".##").format(averageMark);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("avg", parseDouble(formattedAverageMark));
+        return jsonObject;
     }
 }
